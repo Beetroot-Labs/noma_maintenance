908d587c6d49fd3d1cff86b8a285a041cc8a3122
Domonkos Suranyi <domonkos.suranyi@aimotive.com>
2026-01-20 21:09:23 +0100

    Add app images

---
+++ frontend/public/sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = "noma-maintenance-v1";
+const CORE_ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./favicon.ico",
+  "./logo-white.webp",
+  "./app-images/android/android-launchericon-192-192.png",
+  "./app-images/android/android-launchericon-512-512.png",
+  "./app-images/ios/180.png",
+  "./app-images/ios/32.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key)),
+      ),
+    ),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request).then((response) => {
+        const responseClone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+        return response;
+      });
+    }),
+  );
+});
